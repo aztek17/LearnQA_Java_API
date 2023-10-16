4ff648f134f81abc817d8e9726308836e4fb8967
Alexander N <aztek1717@gmail.com>
2023-10-16 12:32:31 +0400

    [test] get list most common passwords

--- src/test/java/PasswordTests.java
+++ src/test/java/PasswordTests.java
@@ -1,11 +1,10 @@
 import io.restassured.RestAssured;
+import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class PasswordTests {
 
@@ -13,14 +12,7 @@ public class PasswordTests {
     public void selectionPassword() {
         String login = "super_admin";
         int iterator = 0;
-        List<String> passwords = Arrays.asList("password", "123456", "12345678", "qwerty", "abc123", "12345", "1234567",
-                "monkey", "123456789", "1234567890", "letmein", "dragon", "baseball", "1234", "sunshine", "iloveyou",
-                "trustno1", "princess", "football", "adobe123[a]", "welcome", "login", "admin", "qwerty123", "solo",
-                "1q2w3e4r", "master", "111111", "666666", "photoshop[a]", "1qaz2wsx", "qwertyuiop", "ashley", "mustang",
-                "121212", "starwars", "654321", "bailey", "access", "flower", "123123", "555555", "passw0rd", "shadow",
-                "lovely", "7777777", "michael", "!@#$%^&*", "jesus", "superman", "hello", "charlie", "888888", "696969",
-                "hottie", "freedom", "aa123456", "qazwsx", "ninja", "azerty", "loveme", "whatever", "donald", "batman",
-                "zaq1zaq1", "000000", "123qwe");
+        List<String> passwords = getMostCommonPasswords();
 
         String message = "You are NOT authorized";
         String password = "";
@@ -49,12 +41,24 @@ public class PasswordTests {
                     .andReturn();
 
             message = validateCookie.asString();
-
             iterator++;
+            System.out.println("Авторизация не пройдена");
         }
 
-        System.out.println("ответ с правильными cookies: " + message);
+        System.out.println("Ответ с правильными cookies: " + message);
         System.out.println("Верный пароль: " + password);
     }
 
+    private ArrayList<String> getMostCommonPasswords() {
+        XmlPath response = RestAssured
+                .given()
+                .get("https://en.wikipedia.org/wiki/List_of_the_most_common_passwords")
+                .htmlPath();
+
+        String allPasswords = response.getString("**.findAll { it.@class == 'wikitable' }[1]");
+        ArrayList<String> listPasswords = new ArrayList<>(Arrays.asList(allPasswords.split("\n")));
+        ArrayList<String> uniquePasswords = (ArrayList<String>) listPasswords.stream().distinct().collect(Collectors.toList());
+        uniquePasswords.removeIf(pass -> (pass.length() < 4 || pass.length() > 10));
+        return uniquePasswords;
+    }
 }
