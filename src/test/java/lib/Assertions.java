c8888a3ac91ad6fcb49d4c07d641cdfc70579076
Alexander N <aztek1717@gmail.com>
2023-11-03 22:57:46 +0400

    [test] refresh

--- src/test/java/lib/Assertions.java
+++ src/test/java/lib/Assertions.java
@@ -3,6 +3,7 @@ package lib;
 import io.restassured.response.Response;
 
 import static org.hamcrest.Matchers.hasKey;
+import static org.hamcrest.Matchers.not;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Assertions {
@@ -12,5 +13,43 @@ public class Assertions {
 
         int value = response.jsonPath().getInt(name);
         assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+    }
+
+    public static void assertJsonByName(Response response, String name, String expectedValue) {
+        response.then().assertThat().body("$", hasKey(name));
+
+        String value = response.jsonPath().getString(name);
+        assertEquals(expectedValue, value, "JSON value is not equal to expected value");
+    }
+
+
+    public static void assertResponseTextEquals(Response response, String expectedAnswer) {
+        assertEquals(
+                expectedAnswer,
+                response.asString(),
+                "Response text is not as expected"
+        );
+    }
+
+    public static void assertResponseCodeEquals(Response response, int expectedStatusCode) {
+        assertEquals(
+                expectedStatusCode,
+                response.statusCode(),
+                "Response status code is not as expected"
+        );
+    }
+
+    public static void assertJsonHasField(Response response, String expectedFieldKey) {
+        response.then().assertThat().body("$", hasKey(expectedFieldKey));
+    }
+
+    public static void assertJsonHasFields(Response response, String[] expectedFieldNames) {
+        for (String expectedFieldName : expectedFieldNames) {
+            Assertions.assertJsonHasField(response, expectedFieldName);
+        }
+    }
+
+    public static void assertJsonHasNotField(Response response, String unexpectedFieldName) {
+        response.then().assertThat().body("$", not(hasKey(unexpectedFieldName)));
     }
 }
